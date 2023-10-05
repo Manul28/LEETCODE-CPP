120174df5bc76b86b641221ab85230214168c1a0
MANUL <113660278+Manul28@users.noreply.github.com>
2023-10-05 22:48:32 +0530

    Update reverse_stringinword.cpp

--- reverse_stringinword.cpp
+++ reverse_stringinword.cpp
@@ -19,5 +19,22 @@ public:
         return ans;
 
         // return ans.substr(0,ans.size()-1);// i removed this bcs of optimization
+        /*
+        string result;
+        int i=0;
+        int n=s.length();
+        while(i<n){
+          while(i<n && s[i]==' ') i++;
+           if(i>=n) break;
+          int j=i+1;
+          while(j<n && s[j]!=' ') j++;
+            string sub=s.substr(i,j-i);
+            if(ans.length()==0) ans=sub;
+            else ans=sub+" "+ans;
+            i=j+1;
+           }
+            return ans;
+        */
+        
     }
 };
