f4a89a23eedc4767783a4beb3f1e55024154ef15
Manul Rastogi <113660278+Manul28@users.noreply.github.com>
2023-10-12 18:59:29 +0530

    Update add_string.java

--- add_string.java
+++ add_string.java
@@ -4,3 +4,9 @@ class Solution {
         return String.valueOf((new BigInteger(num1)).add(new BigInteger(num2)));
     }
 }
+/*
+Approach-
+  -declaring num1 and num2 as BigInteger since they can be easily converted into string 
+  -then using add function we add num1 and num2 and store the result into num1
+  -then using String.valueOf() function we convert very large integer summation into string
+*/
