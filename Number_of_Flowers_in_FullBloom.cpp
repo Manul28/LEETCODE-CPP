d76761e04af311e50706b49123fb229f8b50ffec
Manul Rastogi <113660278+Manul28@users.noreply.github.com>
2023-10-12 10:24:12 +0530

    Update Number_of_Flowers_in_FullBloom.cpp
    Revising the code again so...retyped

--- Number_of_Flowers_in_FullBloom.cpp
+++ Number_of_Flowers_in_FullBloom.cpp
@@ -4,24 +4,23 @@ public:
         ios_base::sync_with_stdio(false);
         cout.tie(NULL);
         cin.tie(NULL);
-        int n= people.size();
-        int m = flowers.size();
+        int m=flowers.size();
+        int n=people.size();
+        vector<int> start(m);
+        vector<int> finish(m);
         vector<int> ans(n,0);
-        vector<int> start(m); // to store start time .
-        vector<int> finish(m); // to store end time
         for(int i=0;i<m;i++){
-           start[i] = flowers[i][0];
-           finish[i] = flowers[i][1];
+            int start=flowers[i][0];
+            int finish=flowers[i][1];
         }
-        sort(start.begin(),start.end()); // start time in ascending order
-        sort(finish.begin(),finish.end()); // end time in ascending order
-     
+        sort(start.begin(),start.end());
+        sort(finish.begin(),finish.end());
+
         for(int i=0;i<n;i++){
-            int target = people[i];
-            int s = upper_bound(start.begin(),start.end(),target)-start.begin();
-            int e = lower_bound(finish.begin(),finish.end(),target)-finish.begin();
-            
-            ans[i] = s-e;;
+            int target=people[i];
+            int s=upper_bound(start.begin(),start.end(),target)-start.begin();
+            int e=lower_bound(finish.begin(),finish.end(),target)-finish.begin();
+            ans[i]=s-e;
         }
         return ans;
         
