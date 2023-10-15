c4feab3f5d124b069aa154d3c4f1e82832d349fd
Manul Rastogi <113660278+Manul28@users.noreply.github.com>
2023-10-15 18:35:11 +0530

    Update Find Indices With Index and Value Difference I.cpp

--- Find Indices With Index and Value Difference I.cpp
+++ Find Indices With Index and Value Difference I.cpp
@@ -1,8 +1,17 @@
-class Solution{
- public int [] findIndices()
+class Solution {
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        int [] answer=new int[2];
+        answer[0]=-1;
+        answer[1]=-1;
+        for(int i=0;i<nums.length-indexDifference;i++){
+            for(int j=i+indexDifference;j<nums.length;j++){
+                if(Math.abs(nums[i]-nums[j])>=valueDifference){
+                    answer[0]=i;
+                    answer[1]=j;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
 }
-for answer view this link 
-https://leetcode.com/contest/weekly-contest-367/submissions/detail/1075498714/
-/*
-I will be uploading the answer tonight
-*/
