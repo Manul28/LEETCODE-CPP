f9ceb413daef884e315a79231d34892536ce0707
Manul Rastogi <113660278+Manul28@users.noreply.github.com>
2023-10-28 10:25:48 +0530

    Update Number of 1 bits.cpp

--- Number of 1 bits.cpp
+++ Number of 1 bits.cpp
@@ -4,7 +4,7 @@ public:
         int count=0;
         while(n!=0){
           count+=(n%2);//counting the least significant bit
-          n>>1;//right shifting
+          n>>=1;//right shifting
         }
         return count;
       }
