ac68d0d011edf404208da1010895cc831a5e4f8b
Manul Rastogi <113660278+Manul28@users.noreply.github.com>
2023-10-28 10:23:22 +0530

    Update Number of 1 bits.cpp

--- Number of 1 bits.cpp
+++ Number of 1 bits.cpp
@@ -2,7 +2,7 @@ class Solution{
 public:
       int hammingweight(uint_32 n){
         int count=0;
-        for(int i=0;i<n;i++){
+        while(n!=0){
           count+=(n%2);//counting the least significant bit
           n>>1;//right shifting
         }
