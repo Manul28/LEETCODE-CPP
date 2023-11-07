b88978499a376edf5e29c39ace862057610370b8
Manul Rastogi <113660278+Manul28@users.noreply.github.com>
2023-11-07 15:58:55 +0530

    Create Greatest common Divisor of string.cpp

---
+++ Greatest common Divisor of string.cpp
@@ -0,0 +1,9 @@
+class Solution{
+public:
+   string gcdofstring(string str1,string str2){
+     ios_base::sync_with_stdio(false);
+     cout.tie(NULL);
+     cin.tie(NULL);
+     return (str1+str2==str2+str1)? str1.substr(0,gcd(size(str1),size(str2)):"";
+   }
+};
